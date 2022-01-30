d45a19b1e7b1e338f65977d2415e0685006d1c77
Christopher Razidlo <cd.razidlo@gmail.com>
2022-01-30 11:14:08 -0600

    added selectedMovie reducer to index.js and redux store

--- src/components/MovieList/MovieListItem/MovieListItem.jsx
+++ src/components/MovieList/MovieListItem/MovieListItem.jsx
@@ -1,8 +1,12 @@
 function MovieListItem({ movie }) {
+
+    const onPosterClick = () => {
+        console.log('in onPosterClick', movie.id );
+    }
     return (
         <div key={movie.id} >
             <h3>{movie.title}</h3>
-            <img src={movie.poster} alt={movie.title}/>
+            <img src={movie.poster} alt={movie.title} onClick={onPosterClick}/>
         </div>
     );
 }
